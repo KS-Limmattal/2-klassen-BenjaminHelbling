a619ae5b873b47754fc1bf5a5d994b35563b5b0a
Benjamin Helbling <benjamin.helbling@stud.kslzh.ch>
2025-11-26 08:40:35 +0100

    26.11.2025

--- Fraction.java
+++ Fraction.java
@@ -2,9 +2,20 @@ public class Fraction {
     static int numberOfFractions;
     private int numerator, denominator;
 
+    //erzeugt die Fraktion (konstruktor)
     public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
+        this.numerator = numerator;                  // this. ersezt ein Objekt wie zum beispiel f. das ist dazu da um verschiedene objekten die gleiche Funktion zu geben bzw die gleiche anwendung.
         this.setDenominator(denominator);
+        numberOfFractions ++;
+    }
+
+    //kopiert eien Fraktion (konstruktor)
+    public Fraction(Fraction f) {               // nicht das gleiche f wie bei der Objekt f. es benötigt einfach ein Objekt aus der Klasse Fraktion
+        this(f.numerator, f.denominator);
+    }
+    //wenn leere Fraktion (konstruktor)
+    public Fraction() {                             // wenn man eine Funktion Fraction() ohne inhalt ausführt ruft es mit "this" die Funktion mit dem gleichen Namen auf aber mit inhalt 0,1 so das es kein error gibt.
+        this(0, 1);       
     }
 
     @Override
@@ -12,19 +23,11 @@ public class Fraction {
         return  numerator +"/" + denominator ;
     }
 
-    public Fraction(Fraction f) {
-        this(f.numerator, f.denominator);
-    }
-
-    public Fraction() {
-        this(0, 1);
-    }
-
-    public int getDenominator() {
+    public int getDenominator() {               // mit dieser funkton kann man uf den denuminator zugreifen in anderen klassen obwohl er auf privat gestellt ist.
         return denominator;
     }
 
-    public void setDenominator(int denominator) {
+    public void setDenominator(int denominator) {                   // mit dieser funktion kann man den denuminator verändern in adneren klassen auch wenn er auf privat gestellt ist
         if (denominator == 0) {
             throw new ArithmeticException("Division by zero");
         } else {
@@ -40,16 +43,16 @@ public class Fraction {
         this.numerator = numerator;
     }
 
-    public void add(Fraction f) {
-        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+    public void add(Fraction f) {       // speichert die adition in dem angewendeten objekt (bsp: g.add(h); wird in g gespeichert)
+        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator); 
         this.setDenominator(this.denominator * f.denominator);
     }
 
-    public static Fraction add(Fraction f, Fraction g) {
-        return new Fraction(f.numerator * g.denominator + f.denominator * g.numerator, f.denominator * g.denominator);
+    public static Fraction add(Fraction f, Fraction g) {               // rechnet die zwei fraktionen zusammen (nenner wird zuerst gleich gemacht). g und f sind nur platzhalter sind nicht die Objekte 
+        return new Fraction(f.numerator * g.denominator + f.denominator * g.numerator, f.denominator * g.denominator); // gibt ein neuers Objekt zurück
     }
     
-    public boolean equals(Fraction f) {
+    public boolean equals(Fraction f) {                                             // prüft auf inhalts gleichheit 
         return this.numerator * f.denominator == this.denominator * f.numerator;
     }
 
--- FractionTester.java
+++ FractionTester.java
@@ -3,6 +3,10 @@ public class FractionTester {
         Fraction f = new Fraction(3,5);
         Fraction g = new Fraction(f);
         Fraction h = new Fraction(f);
+        final double PI = 3.141592;
+        System.out.println(PI);
+        //PI = 3;
+        System.out.println(PI);
         // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
         f.setNumerator(6);
         f.setDenominator(10);
@@ -16,6 +20,7 @@ public class FractionTester {
         //f.setDenominator(3);
         
         System.out.println(h.toString());
+        System.out.println(Fraction.numberOfFractions);
         // Copy-Konstruktor und equals():
         
         // Klassen- und Instanzenmethoden:
